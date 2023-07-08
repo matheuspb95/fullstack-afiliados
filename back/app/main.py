a9717df559d1a04615fc2ac2c931c1a614b3f450
Matheus Palheta Barbosa <matheuspb95@gmail.com>
2023-07-08 09:36:08 -0400

    File data parsing

--- back/app/main.py
+++ back/app/main.py
@@ -1,13 +1,26 @@
+from datetime import datetime
 import uvicorn
 from fastapi import FastAPI, UploadFile
 from sqlalchemy.orm import Session
+
 
 app = FastAPI()
 
 @app.post("/product/file")
 async def create_upload_file(file: UploadFile):
-    contents = file.file.read()
-    return {"contents": contents}
+    file_content = file.file.read().decode('UTF-8')
+    products = []
+    for item in file_content.split('\n'):
+        if(len(item) > 0):
+            products.append({
+                "type": int(item[0]),
+                "date": datetime.strptime(item[1:26], "%Y-%m-%dT%H:%M:%S%z"),
+                "product": item[26:56],
+                "value": float(item[56:66]),
+                "seller": item[66: 86]
+            })
+    
+    return products
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
